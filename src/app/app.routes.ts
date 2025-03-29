bf24ea887a0ee42782d8aca5d6526e8867471e4f
Vamshi Bandela <vamshibandela777@gmail.com>
2025-03-29 07:09:16 +0530

    lazy loading implementation & generating env files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-      path: 'lazy',
-      loadComponent: () =>
-        import('./lazy-load/lazy-load.component').then(
-          (m) => m.LazyLoadComponent
-        ),
-    },
+  {
+    path: 'lazy',
+    loadComponent: () =>
+      import('./lazy-load/lazy-load.component').then(
+        (m) => m.LazyLoadComponent
+      ),
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.routes').then((m) => m.routes),
+  },
 ];
---
+++ src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    basurl: "http://dev.url"
+};
