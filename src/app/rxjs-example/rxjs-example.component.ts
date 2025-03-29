71c4b581acee8de0b1052acd55f6ea30ddba5ab4
Vamshi Bandela <vamshibandela777@gmail.com>
2025-03-29 07:55:15 +0530

    rxjs practice

--- src/app/rxjs-example/rxjs-example.component.ts
+++ src/app/rxjs-example/rxjs-example.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
 import { CapitalizeDirective } from '../drectives/capitalize.directive';
+import { BehaviorSubject, debounceTime, from, interval, of, switchMap } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-rxjs-example',
-  imports: [CapitalizeDirective],
+  imports: [CapitalizeDirective, CommonModule],
   templateUrl: './rxjs-example.component.html',
-  styleUrl: './rxjs-example.component.scss'
+  styleUrl: './rxjs-example.component.scss',
 })
 export class RxjsExampleComponent {
+  sampleObs$ = of(1, 2, 3);
+  timer$ = interval(2000);
+  count = 0;
+  searchData$ = new BehaviorSubject('');
+  apiData$ = of([1,2,3])
 
+  constructor() {
+    this.timer$.subscribe(() => {
+      this.count++;
+      this.sampleObs$ = of(this.count);
+    });
+
+    this.searchData$.pipe(debounceTime(500)).pipe(switchMap((searchText:any)=>{
+      console.log(searchText)
+      return this.apiData$
+    })).subscribe({
+      next: console.log,
+    });
+  }
+
+  onSearch(event: any) {
+    this.searchData$.next(event.target.value);
+  }
 }
